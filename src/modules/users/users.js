07f5556ee532bb2a6d2793b5b536cbc5a9996cad
Kamil Żogło <kamilzoglo@gmail.com>
2019-12-17 19:09:07 +0100

    Uncomment of first line in users.js and first two lines in index.js. Restoration of the pulled state of affairs. App worked only with these lines being commented out.

--- src/modules/users/users.js
+++ src/modules/users/users.js
@@ -1,129 +1,136 @@
-import { userInfo } from "os";
+import { userInfo } from 'os';
 
 const urlServer = 'http://127.0.0.1:3000/api/';
 // var id = '';
 // var token = '';
 
-export const login = async function(city){
-    $('.showhome').hide();
-    $('.content').show();
-    $.get('/src/modules/users/templates/login.mst', function(template) {
-        const result = Mustache.to_html(template);
-        $('.content').html(result);
-        doAfterLog();
-    });
+export const login = async function(city) {
+  $('.showhome').hide();
+  $('.content').show();
+  $.get('/src/modules/users/templates/login.mst', function(template) {
+    const result = Mustache.to_html(template);
+    $('.content').html(result);
+    doAfterLog();
+  });
 };
 
-export const register = async function(city){
-    $('.showhome').hide();
-    $('.content').show();
-    $.get('/src/modules/users/templates/register.mst', function(template) {
-        const result = Mustache.to_html(template);
-        $('.content').html(result);
-        doAfterReg();
-    });
+export const register = async function(city) {
+  $('.showhome').hide();
+  $('.content').show();
+  $.get('/src/modules/users/templates/register.mst', function(template) {
+    const result = Mustache.to_html(template);
+    $('.content').html(result);
+    doAfterReg();
+  });
 };
 
-export const showDetails = async function(city){
-    $('.showhome').hide();
-    $('.content').show();
-    doAfterSD();
+export const showDetails = async function(city) {
+  $('.showhome').hide();
+  $('.content').show();
+  doAfterSD();
 };
 
 //register
 
 function doAfterReg() {
-    document.getElementById('register-btn').addEventListener('click', postUser);
+  document.getElementById('register-btn').addEventListener('click', postUser);
 }
 
-function doAfterLog(){
-    document.getElementById('login-btn').addEventListener('click', getToken);
-    document.getElementById('register-login').addEventListener('click', register);
+function doAfterLog() {
+  document.getElementById('login-btn').addEventListener('click', getToken);
+  document.getElementById('register-login').addEventListener('click', register);
 }
 
-function doAfterSD(){
-    fetch(urlServer+'users/'+id, {method: 'GET',})
+function doAfterSD() {
+  fetch(urlServer + 'users/' + id, { method: 'GET' })
     .then(data => data.json())
     .then(user => {
-        $.get('/src/modules/users/templates/showDetails.mst', function(template) {
-            const result = Mustache.to_html(template, user);
-            $('.content').html(result);
-        });
+      $.get('/src/modules/users/templates/showDetails.mst', function(template) {
+        const result = Mustache.to_html(template, user);
+        $('.content').html(result);
+      });
     })
-    .catch(err => {console.log(err);});
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 function getToken() {
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    console.log(` e ${email} p ${password} `);
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  console.log(` e ${email} p ${password} `);
 
-    const User = {
-        'email': email,
-        'password': password
-    }
+  const User = {
+    email: email,
+    password: password
+  };
 
-    const otherParam = {
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(User),
-        method: 'POST'
-    }
+  const otherParam = {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(User),
+    method: 'POST'
+  };
 
-    fetch(urlServer+'auth', otherParam)
+  fetch(urlServer + 'auth', otherParam)
     .then(data => data.json())
     .then(data => {
-        localStorage.setItem('token', data.token);
-        localStorage.setItem('user', JSON.stringify(data.user));
-        let name = JSON.parse(localStorage.getItem('user')).name;
-        let surname = JSON.parse(localStorage.getItem('user')).surname;
-        document.getElementById('user-email').innerText = `${name} ${surname} `;
-        document.getElementById('login').style.display = "none";
-        document.getElementById('register').style.display = "none";    
-        document.getElementById('member-nav').style.display = "unset";
-        document.getElementById('project-nav').style.display = "unset";
-        document.getElementById('tasks-nav').style.display = "unset";
-        document.getElementById('user-email-nav').style.display = "unset";
-        document.getElementById("list-projects").click();
-
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('user', JSON.stringify(data.user));
+      let name = JSON.parse(localStorage.getItem('user')).name;
+      let surname = JSON.parse(localStorage.getItem('user')).surname;
+      document.getElementById('user-email').innerText = `${name} ${surname} `;
+      document.getElementById('login').style.display = 'none';
+      document.getElementById('register').style.display = 'none';
+      document.getElementById('member-nav').style.display = 'unset';
+      document.getElementById('project-nav').style.display = 'unset';
+      document.getElementById('tasks-nav').style.display = 'unset';
+      document.getElementById('user-email-nav').style.display = 'unset';
+      document.getElementById('list-projects').click();
     })
-    .catch(err => {console.log(err);});
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 function postUser() {
-    console.log('register');
-    let name = document.getElementById('name').value;
-    let surname = document.getElementById('surname').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let password2 = document.getElementById('password2').value;
-    console.log(` n ${name} s ${surname} e ${email} p ${password} p2 ${password2}`);
+  console.log('register');
+  let name = document.getElementById('name').value;
+  let surname = document.getElementById('surname').value;
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  let password2 = document.getElementById('password2').value;
+  console.log(
+    ` n ${name} s ${surname} e ${email} p ${password} p2 ${password2}`
+  );
 
-    if(password !== password2) {
-        document.getElementById('msg').innerText= 'Hasła muszą być identyczne.';
-    } else {
-        document.getElementById('msg').innerText= '';
-        const User = {
-            'name': name,
-            'surname': surname,
-            'email': email,
-            'password': password
-        }
+  if (password !== password2) {
+    document.getElementById('msg').innerText = 'Hasła muszą być identyczne.';
+  } else {
+    document.getElementById('msg').innerText = '';
+    const User = {
+      name: name,
+      surname: surname,
+      email: email,
+      password: password
+    };
 
-        const otherParam = {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(User),
-            method: 'POST'
-        }
+    const otherParam = {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(User),
+      method: 'POST'
+    };
 
-        fetch(urlServer+'users', otherParam)
-        .then(data => data.json())
-        .then(user => {console.log(user);})
-        .catch(err => {console.log(err);});
-
-    }
+    fetch(urlServer + 'users', otherParam)
+      .then(data => data.json())
+      .then(user => {
+        console.log(user);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
-
