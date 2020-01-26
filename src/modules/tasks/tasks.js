316065c80be00b9fa8a2b5be9b4f5d64f4211aa2
Kamil Żogło <kamilzoglo@gmail.com>
2020-01-26 20:08:54 +0100

    Broken paths' fix testing v2.

--- src/modules/tasks/tasks.js
+++ src/modules/tasks/tasks.js
@@ -24,31 +24,27 @@ export const addTask = async function() {
   $('.showhome').hide();
   $('.content').show();
 
-  $.get(
-    `CodersCamp_Todolist_Frontend/modules/tasks/templates/addTask/addTask.mst`,
-    function(template) {
-      const result = Mustache.render(template);
-      $('.content').html(result);
-      loadingPage();
-      validationInput();
-      createTask();
-    }
-  );
+  $.get(`${appName}/modules/tasks/templates/addTask/addTask.mst`, function(
+    template
+  ) {
+    const result = Mustache.render(template);
+    $('.content').html(result);
+    loadingPage();
+    validationInput();
+    createTask();
+  });
 };
 
 export const tasksList = async function() {
   $('.showhome').hide();
   $('.content').show();
 
-  $.get(
-    `$CodersCamp_Todolist_Frontend/modules/tasks/templates/tasksList/tasksList.mst`,
-    function(template) {
-      const result = Mustache.render(template);
-      $('.content').html(result);
-      renderTasksList();
-      displayTaskDetails();
-      startTask();
-      closeTasksDetails();
-    }
-  );
+  $.get(`modules/tasks/templates/tasksList/tasksList.mst`, function(template) {
+    const result = Mustache.render(template);
+    $('.content').html(result);
+    renderTasksList();
+    displayTaskDetails();
+    startTask();
+    closeTasksDetails();
+  });
 };
